ff45474cf65d66715ca81e93da87f795a008645f
ivan tkachenko <ratijas@users.noreply.github.com>
2016-02-29 16:57:30 +0800

    remove unnecessary code and prettify bkrs filter

--- src/bkrs.js
+++ src/bkrs.js
@@ -1,39 +1,26 @@
-
 /* заточено под bkrs.info */
-(function (window) {
-	/* ветка, с которой начинать поиск; приватная переменная */
-	var rootNode;
+window.ColorizeFilter = {
+    rootNode: function () {
+        return window.document.getElementById('ajax_search');
+    },
 
-	window.ColorizeFilter = {
-		/* доступ к приватной переменной */
-		rootNode: function () {
-			/* ленивая инициализация */
-			if (!rootNode)
-				/* эта функция должна быть вызвана после загрузки dom */
-				rootNode = document.getElementById('ajax_search');
-			return rootNode;
-		}, // конец rootNode()
+    // return false, if node should not be parsed
+    filterNode: function (node) {
 
-		/* фильтрует ветки; возвращает false, если ветка не подходит */
-		filterNode: function (node) {
-			/* результат сравнений; по умолчанию -- подходит */
-			var ok = true;
+        var tagName = node.tagName.toLowerCase();
+        var id = node.id;
+        var style = node.getAttributeNode( 'style' );
 
-			var tagName = node.tagName.toLowerCase();
-			var id = node.id;
-			var style = node.getAttributeNode( 'style' );
-
-			/* пропустить теги <a>, <input>, <span style="color:brown">, *[id="userName"] */
-			if ( tagName == 'a' ||
-				 tagName == 'input' ||
-				 ( tagName == 'span' &&
-				 	!!style &&
-				 	style.nodeValue == 'color:brown' ) ||
-				 id == 'userName' ){
-				/* не подходит */
-				ok = false;
-			};
-			return ok;
-		} // конец filterNode()
-	}; // конец window.Colorize
-})(window);
+        // skip <a>, <input>, <span style="color:brown">, *[id="userName"]
+        if ( tagName == 'a' ||
+             tagName == 'input' ||
+             ( tagName == 'span' &&
+                !!style &&
+                style.nodeValue == 'color:brown' ) ||
+             id == 'userName' )
+        {
+            return false;
+        }
+        return true;
+    }
+};
